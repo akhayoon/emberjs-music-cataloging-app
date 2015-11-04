e8db2cdcdf39efad25348dff6dd7d0b980fede2e
Hussien <akhayoon@gmail.com>
2015-11-04 12:03:09 -0800

    We now have the ability to add new ratings each song

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   tagName:     'div',
   classNames:  ['rating-panel'],
 
+
   stars: Ember.computed('rating', 'maxRating', function() {
     var fullStars = this.starRange(1, this.get('rating'), 'full');
     var emptyStars = this.starRange(this.get('rating') + 1,
@@ -22,5 +23,14 @@ export default Ember.Component.extend({
   rating:      0,
   maxRating:   5,
   item:        null,
-  setAction:   '',
+  "on-click":  '',
+
+  actions: {
+    set: function(newRating){
+      this.get('on-click') ({
+        item: this.get('item'),
+        rating: newRating
+      });
+    }
+  }
 });
