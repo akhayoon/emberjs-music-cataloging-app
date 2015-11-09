d6819062e44ec1c090fc800f0379e9d4499694cb
Hussien <akhayoon@gmail.com>
2015-11-09 07:57:35 -0800

    added a capitilization helper and applied it to template/controller/route

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { capitalize as capitalizeWords } from '../../../helpers/capitalize';
 
 export default Ember.Route.extend({
   model: function() {
@@ -21,7 +22,8 @@ export default Ember.Route.extend({
 
     didTransition: function() {
       var band = this.modelFor('bands.band');
-      document.title = `${band.get('name')} songs - Rock & Roll`;
+      var name = capitalizeWords(band.get('name'));
+      document.title = `${name} songs - Rock & Roll`;
     },
   }
 });
